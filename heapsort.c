98fa6e278e49ee0b04d97b6add475ceebff0a362
Prithvi Chauhan <51173793+chauhan0707@users.noreply.github.com>
2020-11-23 23:50:20 +0530

    Add files via upload

--- heapsort.c
+++ heapsort.c
@@ -1,72 +1,54 @@
-#include <math.h> 
-#include <stdio.h> 
-#include <stdlib.h>
-#include <time.h>
-int temp;  
+#include <stdio.h>
+  int main()
+  {
+  int arr[10], no,  i, j, c, heap_root, temp;
+  printf("Input number of elements: ");
+  scanf("%d", &no);
+  printf("\nInput array values one by one : ");
+  for (i = 0; i <  no; i++)
+  scanf("%d", &arr[i]);
+  for (i = 1; i <  no; i++)
+  {
+  c = i;
+  do
+  {
+  heap_root  = (c - 1) / 2;
+  /* to create MAX arr  array */
+  if  (arr[heap_root] < arr[c])
+  {
+  temp =  arr[heap_root];
+  arr[heap_root] = arr[c];
+  arr[c]  = temp;
+  }
+  c =  heap_root;
+  } while (c !=  0);
+  }
   
-void heapify(int arr[], int size, int i)  
-{  
-int largest = i;    
-int left = 2*i + 1;    
-int right = 2*i + 2;    
-  
-if (left < size && arr[left] >arr[largest])  
-largest = left;  
-  
-if (right < size && arr[right] > arr[largest])  
-largest = right;  
-  
-if (largest != i)  
-{  
-temp = arr[i];  
-    arr[i]= arr[largest];   
-    arr[largest] = temp;  
-heapify(arr, size, largest);  
-}  
-}  
-  
-void heapSort(int arr[], int size)  
-{  
-int i;  
-for (i = size / 2 - 1; i >= 0; i--)  
-heapify(arr, size, i);  
-for (i=size-1; i>=0; i--)  
-{  
-temp = arr[0];  
-    arr[0]= arr[i];   
-    arr[i] = temp;  
-heapify(arr, i, 0);  
-}  
-}   
-  
-
-int main() 
-{ 
-	
-    int n,t;
-    printf("Enter No of testcases : ");
-    scanf("%d", &t);
-    while(t--){
-    	clock_t start, end;
-	    double cpu_time_used;
-	     
-	    start = clock();
-	    
-	    printf("n = ");
-	    scanf("%d", &n);
-    	int i,arr[n];
-		for(i=0;i<n;i++)
-			arr[i] = rand()%100;
-			
-		heapSort(arr,n); 
-		
-		
-		end = clock();
-	    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-     	printf("time = %.5f \n", cpu_time_used);
-	}
-	
-
-	return 0; 
-} 
-
+  printf("Heap  array : ");
+  for (i = 0; i <  no; i++)
+  printf("%d\t ", arr[i]);
+  for (j = no - 1; j  >= 0; j--)
+  {
+  temp = arr[0];
+  arr[0] = arr[j];
+  arr[j] = temp;
+  heap_root = 0;
+  do
+  {
+  c = 2 *  heap_root + 1;
+  if  ((arr[c] < arr[c + 1]) && c < j-1)
+  c++;
+  if  (arr[heap_root]<arr[c] && c<j)
+  {
+  temp =  arr[heap_root];
+  arr[heap_root] = arr[c];
+  arr[c]  = temp;
+  }
+  heap_root  = c;
+  } while (c  < j);
+  } 
+  printf("\nSorted  array : ");
+  for (i = 0; i <  no; i++)
+  printf("\t%d", arr[i]);
+  printf("\n");
+  }
