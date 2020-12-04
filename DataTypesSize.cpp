6282f83bc1ec022e573569f48af3b6c43bed161b
Prithvi Chauhan <51173793+chauhan0707@users.noreply.github.com>
2020-12-04 23:42:39 +0530

    Add files via upload

---
+++ DataTypesSize.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<sizeof(char)<<endl; // In bytes
+	char c;
+	cout<<sizeof(c)<<endl;
+	cout<<sizeof(short)<<endl;
+	cout<<sizeof(int)<<endl;
+	cout<<sizeof(float)<<endl;
+	cout<<sizeof(long long int)<<endl;
+	cout<<sizeof(double)<<endl;
+	return 0;
+}
