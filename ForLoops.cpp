beca920906ad10c3e0a280cbe6fafad0ad509cb9
Prithvi Chauhan <51173793+chauhan0707@users.noreply.github.com>
2020-12-05 23:56:52 +0530

    Add files via upload

---
+++ ForLoops.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	for(int i=1;i<=10;i++)
+	{
+		cout<<"GFG";   // We can use '\n' or endl for going to a new line.
+		cout<<endl;
+	}
+return 0;
+}
