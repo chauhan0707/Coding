75c661d9e46e389b30b20978a099b592a4e99820
Prithvi Chauhan <51173793+chauhan0707@users.noreply.github.com>
2021-01-22 23:50:38 +0530

    Add files via upload

---
+++ HelloWorld.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"Hello World\n"; // \n is for line break, jumping to next line.
+	return 0;
+}
+// - Single Line comment.
+/* Multi line comment. */
+/** Documentation */ 
