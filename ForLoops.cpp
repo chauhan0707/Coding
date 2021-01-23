606a16d1859efb1ca1e742ede879a4fd8425493b
Prithvi Chauhan <51173793+chauhan0707@users.noreply.github.com>
2021-01-23 23:46:33 +0530

    Add files via upload

--- ForLoops.cpp
+++ ForLoops.cpp
@@ -2,10 +2,19 @@
 using namespace std;
 int main()
 {
-	for(int i=1;i<=10;i++)
-	{
-		cout<<"GFG";   // We can use '\n' or endl for going to a new line.
-		cout<<endl;
-	}
-return 0;
+	int i;
+	for(i=0;i<10;i++)
+	cout<<"GFG"<<endl;
+	cout<<i;
+	return 0;
 }
+/*
+for(int i=0;i<10;i++)
+cout<<"GFG";
+cout<<i; error as i is defined for the loop. Its scope is only for there.
+
+int i;
+for(i=0;i<10;i++)
+cout<<"GFG";
+cout<<i; 10 will be the output. As the loop terminated when the i become 10 so outside its scope i will be 10.
+*/
